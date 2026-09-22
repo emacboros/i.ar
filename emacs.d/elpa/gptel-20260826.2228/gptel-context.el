f731b941159c563d0ea04f5e7d2e24f42346eb11
aria-agent <aria@emacboros.local>
2026-09-22 17:11:38 +0000

    elpa gptel-context: sync shadow to fork (continuo 33b0f9c helper; belt v2 caught the divergence)

--- emacs.d/elpa/gptel-20260826.2228/gptel-context.el
+++ emacs.d/elpa/gptel-20260826.2228/gptel-context.el
@@ -842,6 +842,9 @@ If non-nil, indicates backward movement.")
                           (overlays-in (point-min) (point-max))))))
     (mapc #'gptel-context-remove deletion-marks)
     (revert-buffer))
+  ;; FIXME(context): This should run in the buffer from which the context
+  ;; inspection buffer was visited.
+  ;; Update contexts and revert buffer (#482)
   (setq gptel-context (nreverse (gptel-context--collect)))
   (gptel-context-quit))
 
